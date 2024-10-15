94d03f8ac71b68c78d40cb434032011248d875d5
Hrushikesh <hrushikeshsurkar@gmial.com>
2024-10-15 08:16:56 +0530

    working on primary user details page

--- src/components/Profile/primary-user-details/PrimaryUserDetails.jsx
+++ src/components/Profile/primary-user-details/PrimaryUserDetails.jsx
@@ -1,39 +1,34 @@
-import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
-import AuthHook from "../../../auth/AuthHook";
-import { useParams } from "react-router-dom";
-import Swal from "sweetalert2";
-import { RingLoader } from "react-spinners";
 import "./PrimaryUserDetails.css";
-import ImageCard from "./ImageCard";
-// Fields array
-export const fields = [
-  { label: "First Name", key: "firstName" },
-  { label: "Last Name", key: "lastName" },
-  { label: "Age", key: "age" },
-  { label: "Gender", key: "gender" },
-  { label: "Language Known", key: "langKnown" },
-  { label: "Religion", key: "religion" },
-  { label: "Community", key: "community" },
-  { label: "Date of Birth", key: "dob" },
-  { label: "Residence", key: "residence" },
-  { label: "Mobile No", key: "mobileNumber", isDisabled: true },
-  { label: "Email Id", key: "mailId" },
-];
+import { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+import { Modal } from "react-bootstrap";
+import { FaRegEdit } from "react-icons/fa";
 
-// Main Component
 const PrimaryUserDetails = ({
-  response,
-  refresAfterUpdate,
-  setStatus,
   status,
+  setStatus,
+  response,
+  mobileNumber,
+  imageUrl,
 }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [updatedProfile, setUpdatedProfile] = useState(response || {});
+  const [updatedProfile, setUpdatedProfile] = useState({
+    firstName: "",
+    lastName: "",
+    age: "",
+    gender: "",
+    languageKnown: "",
+    religion: "",
+    community: "",
+    dob: "",
+    residence: "",
+    mobileNumber: "",
+    mailId: "",
+    profileImage: "",
+  });
+
   const [loading, setLoading] = useState(false);
   const [profileImage, setProfileImage] = useState(null);
-  const session = AuthHook();
-  const { mobileNumber } = useParams();
 
   useEffect(() => {
     setUpdatedProfile(response);
@@ -74,7 +69,6 @@ const PrimaryUserDetails = ({
         setLoading(false);
         if (data.status === 200 || data.status === 201) {
           setStatus(!status);
-          refresAfterUpdate && refresAfterUpdate(!status);
 
           if (data.profileImage) {
             setUpdatedProfile((prevProfile) => ({
@@ -110,130 +104,120 @@ const PrimaryUserDetails = ({
   };
 
   return (
-    <>
-      <ImageCard setStatus={setStatus} mobileNumber={mobileNumber} />
-      <motion.div
-        className="card-container"
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        {mobileNumber === session?.userName && (
-          <div className="button-container">
-            <button className="btn btn-primary" onClick={toggleModal}>
-              Update
-            </button>
-          </div>
-        )}
-
-        <div className="content-wrapper">
-          {fields.map((field, index) => (
-            <div className="field" key={index}>
-              <strong>{field.label}:</strong>{" "}
-              <span style={{ color: "#003566" }}>
-                {response && response[field.key] !== undefined
-                  ? Array.isArray(response[field.key])
-                    ? response[field.key].join(", ")
-                    : response[field.key]
-                  : "N/A"}
-              </span>
-            </div>
-          ))}
+    <section className="primary-user-details-wrap">
+      <div className="update-button">
+        <FaRegEdit className="icon" onClick={toggleModal} disabled={loading} />
+      </div>
+      <div className="first-row-wrap">
+        <div className="profile-image-wrap">
+          <img src={imageUrl} alt="profile-image" className="profile-image" />
+          <span>
+            {response?.firstName}
+            {response?.lastName}
+          </span>
         </div>
-      </motion.div>
+      </div>
+      <div className="other-information">
+        <div className="name-wrap">
+          <h3 className="name"> Age: {response?.age}</h3>
+          <h3 className="name"> Gender: {response?.gender}</h3>
+          <h3 className="name"> Language Known: {response?.languageKnown}</h3>
+          <h3 className="name"> Religion : {response?.religion}</h3>
+          <h3 className="name"> Community : {response?.community}</h3>
+          <h3 className="name"> Date of Birth: {response?.dob}</h3>
+          <h3 className="name"> Address: {response?.residence}</h3>
+          <h3 className="name"> Mobile No: {response?.mobileNumber}</h3>
+          <h3 className="name"> Email: {response?.mailId}</h3>
+        </div>
+      </div>
 
-      {isModalOpen && (
-        <div className="modal-overlay">
-          <div className="modal-content">
-            <h2 className="modal-header">Update Profile</h2>
-            <div className="form-wrapper">
-              {fields.map((field, index) => (
-                <div className="input-field" key={index}>
-                  <label>{field.label}</label>
-                  {field.key === "religion" || field.key === "community" ? (
-                    <select
-                      className="form-control"
-                      value={updatedProfile[field.key] || ""}
-                      onChange={(e) =>
-                        handleFieldChange(field.key, e.target.value)
-                      }
-                    >
-                      {field.key === "religion" && (
-                        <>
-                          <option value="">Select Religion</option>
-                          <option value="Hindu">Hindu</option>
-                          <option value="Muslim">Muslim</option>
-                          <option value="Christian">Christian</option>
-                          <option value="Sikh">Sikh</option>
-                          <option value="Parsi">Parsi</option>
-                          <option value="Jain">Jain</option>
-                          <option value="Buddhist">Buddhist</option>
-                          <option value="Jewish">Jewish</option>
-                          <option value="No Religion">No Religion</option>
-                        </>
-                      )}
-                      {field.key === "community" && (
-                        <>
-                          <option value="">Select Community</option>
-                          <option value="English">English</option>
-                          <option value="Hindi">Hindi</option>
-                          <option value="Urdu">Urdu</option>
-                          <option value="Telugu">Telugu</option>
-                          <option value="Tamil">Tamil</option>
-                        </>
-                      )}
-                    </select>
-                  ) : (
-                    <input
-                      type={field.key === "mobileNumber" ? "text" : "text"}
-                      className="form-control"
-                      value={updatedProfile[field.key] || ""}
-                      onChange={(e) =>
-                        handleFieldChange(field.key, e.target.value)
-                      }
-                      disabled={field.isDisabled}
-                    />
-                  )}
-                </div>
-              ))}
-              {/* Image Upload Input */}
-              <div className="input-field">
-                <label>Profile Image:</label>
+      {response && !loading && isModalOpen && (
+        <Modal
+          show={isModalOpen}
+          onHide={toggleModal}
+          className="modal-dialog modal-dialog-centered"
+        >
+          <Modal.Header closeButton>Update Profile</Modal.Header>
+          <Modal.Body>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit();
+              }}
+            >
+              <div className="form-group">
+                <label htmlFor="firstName">First Name</label>
                 <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
+                  type="text"
                   className="form-control"
+                  id="firstName"
+                  name="firstName"
+                  value={updatedProfile.firstName}
+                  onChange={(e) =>
+                    handleFieldChange("firstName", e.target.value)
+                  }
                 />
               </div>
-            </div>
-
-            {loading ? (
-              <div className="loader-container">
-                <RingLoader color="#003566" size={60} />
+              <div className="form-group">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="lastName"
+                  name="lastName"
+                  value={updatedProfile.lastName}
+                  onChange={(e) =>
+                    handleFieldChange("lastName", e.target.value)
+                  }
+                />
               </div>
-            ) : (
-              <div className="message"></div>
-            )}
-            <br />
-            <button
-              className="btn btn-success"
-              onClick={handleSubmit}
-              disabled={loading}
-            >
-              Save Changes
-            </button>
-            <button
-              className="btn btn-secondary"
-              onClick={toggleModal}
-              style={{ marginLeft: "5px" }}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
+              <div className="form-group">
+                <label htmlFor="profileImage">Profile Image</label>
+                <input
+                  type="file"
+                  className="form-control"
+                  id="profileImage"
+                  name="profileImage"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
+              </div>
+              {/* Remove the duplicated profile image input */}
+              <div className="form-group">
+                <label htmlFor="age">Age</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="age"
+                  name="age"
+                  value={updatedProfile.age}
+                  onChange={(e) => handleFieldChange("age", e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="gender">Gender</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="gender"
+                  name="gender"
+                  value={updatedProfile.gender}
+                  onChange={(e) => handleFieldChange("gender", e.target.value)}
+                />
+              </div>
+              {/* Add remaining fields as necessary */}
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={loading}
+              >
+                {loading ? "Loading..." : "Update"}
+              </button>
+            </form>
+          </Modal.Body>
+        </Modal>
       )}
-    </>
+    </section>
   );
 };
 

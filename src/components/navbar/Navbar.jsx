5be5f151b047bbe13f74694cf1dccb57b68702d5
Hrushikesh <hrushikeshsurkar@gmial.com>
2024-10-19 23:42:11 +0530

    working on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
-import { useTheme, useMediaQuery } from "@mui/material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom"; // Import useLocation
 import { motion } from "framer-motion";
 import styled from "styled-components";
 import AuthHook from "../../auth/AuthHook";
-import Swal from "sweetalert2"; // Import SweetAlert
+import Swal from "sweetalert2";
 import "./Navbar.css";
+import {
+  FaUser,
+  FaTachometerAlt,
+  FaKey,
+  FaSignOutAlt,
+  FaHome,
+  FaSignInAlt,
+  FaUserPlus,
+} from "react-icons/fa"; // Import icons
 
 const Loader = styled(motion.div)`
   position: fixed;
@@ -39,44 +47,24 @@ const LoaderAnimation = styled(motion.div)`
 `;
 
 function Navbar() {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const [loading, setLoading] = useState(true);
-  const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
-  const [toggleNavbarColor, setToggleNavbarColor] = useState(false);
+  const [activeLink, setActiveLink] = useState(""); // Track active link
   const session = AuthHook();
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(() => {
-    const userInfo = localStorage.getItem("userInfo");
-    return userInfo !== null;
-  });
+  const location = useLocation(); // Get the current location
+  const [toggleNavbarColor, setToggleNavbarColor] = useState(
+    !!session?.jwtToken
+  );
 
   useEffect(() => {
-    setToggleNavbarColor(session?.jwtToken ? true : false);
+    setToggleNavbarColor(!!session?.jwtToken);
   }, [session]);
 
+  // Set the default active link based on the current location
   useEffect(() => {
-    const handleScroll = () => {
-      setScrolled(window.scrollY > 0);
-    };
+    setActiveLink(location.pathname);
+  }, [location.pathname]); // Update activeLink when the path changes
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  // Simulate a loading delay
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 1000); // 1 second delay
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  // Handle Logout
   const handleLogout = () => {
     Swal.fire({
       title: "Are you sure?",
@@ -91,112 +79,211 @@ function Navbar() {
         localStorage.removeItem("userInfo");
         Swal.fire({
           title: "Logout Successfully",
-          text: "You have been logout successfully!",
+          text: "You have been logged out successfully!",
           icon: "success",
           confirmButtonText: "OK",
         }).then(() => {
-          setIsLoggedIn(false);
           navigate("/");
-        });
-      } else {
-        Swal.fire({
-          title: "Logout Failed",
-          text: response.data.message || "Something went wrong.",
-          icon: "error",
-          confirmButtonText: "OK",
         });
       }
     });
-    // setIsLoggedIn(false);
-    // navigate("/login");
   };
 
   const handleProfileClick = () => {
     if (session?.userName) {
+      setActiveLink("profile");
       navigate(`/all-details/${session.userName}`);
+      setMenuOpen(false); // Close the sidebar
     }
   };
 
-  if (loading) {
-    return (
-      <Loader
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.3 }}
-      >
-        <LoaderAnimation />
-      </Loader>
-    );
-  }
+  const handleNavigation = (link) => {
+    setActiveLink(link);
+    setMenuOpen(false);
+    navigate(link);
+  };
 
   return (
-    <nav
-      className={`navbar navbar-expand-lg ${
-        toggleNavbarColor ? "nav-bg-white" : "nav-bg-transparent"
-      }`}
-    >
-      <div className="container">
-        <div className="navbar-brand">Marriage Harmony</div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="navbar-toggler-icon "></span>
-        </button>
-        <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}>
-          <ul className="navbar-nav ms-auto ">
-            {session?.jwtToken ? (
-              <>
-                <li className="nav-item">
-                  <button className="nav-link" onClick={handleProfileClick}>
-                    My Profile
-                  </button>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profiles">
-                    Dashboard
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/change-password">
-                    Change Password
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" onClick={handleLogout}>
-                    Logout
-                  </Link>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/register">
-                    Register
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul>
+    <>
+      <nav
+        className={`navbar navbar-expand-lg ${
+          toggleNavbarColor ? "nav-bg-white" : "nav-bg-transparent"
+        }`}
+      >
+        <div className="container">
+          <div className="navbar-brand">Marriage Harmony</div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`}>
+            <ul className="navbar-nav ms-auto">
+              {session?.jwtToken ? (
+                <>
+                  <li className="nav-item">
+                    <button
+                      className={`nav-link ${
+                        activeLink === "profile" ? "active" : ""
+                      }`}
+                      onClick={handleProfileClick}
+                    >
+                      <span className="nav-icon desktop-only"></span>
+                      My Profile
+                    </button>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link ${
+                        activeLink === "/profiles" ? "active" : ""
+                      }`}
+                      to="/profiles"
+                      onClick={() => handleNavigation("/profiles")}
+                    >
+                      <span className="nav-icon desktop-only"></span>
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link ${
+                        activeLink === "/change-password" ? "active" : ""
+                      }`}
+                      to="/change-password"
+                      onClick={() => handleNavigation("/change-password")}
+                    >
+                      <span className="nav-icon desktop-only"></span>
+                      Change Password
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" onClick={handleLogout}>
+                      <span className="nav-icon desktop-only"></span>
+                      Logout
+                    </Link>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link ${
+                        activeLink === "/" ? "active" : ""
+                      }`}
+                      to="/"
+                      onClick={() => handleNavigation("/")}
+                    >
+                      <span className="nav-icon desktop-only"></span>
+                      Home
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link ${
+                        activeLink === "/login" ? "active" : ""
+                      }`}
+                      to="/login"
+                      onClick={() => handleNavigation("/login")}
+                    >
+                      <span className="nav-icon desktop-only"></span>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link ${
+                        activeLink === "/register" ? "active" : ""
+                      }`}
+                      to="/register"
+                      onClick={() => handleNavigation("/register")}
+                    >
+                      <span className="nav-icon desktop-only"></span>
+                      Register
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
         </div>
+      </nav>
+
+      <div className={`responsive-sidebar ${menuOpen ? "open" : ""}`}>
+        <div className="sidebar-header-wrap">
+          <div className="navbar-brand">Marriage Harmony</div>
+          <button className="close-button" onClick={() => setMenuOpen(false)}>
+            &times;
+          </button>
+        </div>
+        <ul className="mobile-menu">
+          {session?.jwtToken ? (
+            <>
+              <li
+                className={`mobile-link ${
+                  activeLink === "profile" ? "active" : ""
+                }`}
+                onClick={handleProfileClick}
+              >
+                <FaUser className="mobile-icon" />
+                My Profile
+              </li>
+              <li
+                className={`mobile-link ${
+                  activeLink === "/profiles" ? "active" : ""
+                }`}
+                onClick={() => handleNavigation("/profiles")}
+              >
+                <FaTachometerAlt className="mobile-icon" />
+                Dashboard
+              </li>
+              <li
+                className={`mobile-link ${
+                  activeLink === "/change-password" ? "active" : ""
+                }`}
+                onClick={() => handleNavigation("/change-password")}
+              >
+                <FaKey className="mobile-icon" />
+                Change Password
+              </li>
+              <li className="mobile-link" onClick={handleLogout}>
+                <FaSignOutAlt className="mobile-icon" />
+                Logout
+              </li>
+            </>
+          ) : (
+            <>
+              <li
+                className={`mobile-link ${activeLink === "/" ? "active" : ""}`}
+                onClick={() => handleNavigation("/")}
+              >
+                <FaHome className="mobile-icon" />
+                Home
+              </li>
+              <li
+                className={`mobile-link ${
+                  activeLink === "/login" ? "active" : ""
+                }`}
+                onClick={() => handleNavigation("/login")}
+              >
+                <FaSignInAlt className="mobile-icon" />
+                Login
+              </li>
+              <li
+                className={`mobile-link ${
+                  activeLink === "/register" ? "active" : ""
+                }`}
+                onClick={() => handleNavigation("/register")}
+              >
+                <FaUserPlus className="mobile-icon" />
+                Register
+              </li>
+            </>
+          )}
+        </ul>
       </div>
-    </nav>
+    </>
   );
 }
 
